38b9dc65b2b98efd24e08aa7f82055e8fc4a3786
sjacob <nebula032@naver.com>
2023-08-26 01:03:33 +0900

    Chapter05 시큐리티 설정

---
+++ src/main/java/com/sjacob/book/springboot/config/auth/SessionUser.java
@@ -0,0 +1,17 @@
+package com.sjacob.book.springboot.config.auth;
+
+import com.sjacob.book.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
