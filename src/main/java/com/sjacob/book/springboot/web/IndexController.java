0a7f82ee4e78adfe3aab8359cffc9dcf6cfa12e9
sjacob <nebula032@naver.com>
2023-08-31 15:51:38 +0900

    Chapter05 5-4 어노테이션 기반으로 개선 5-5 세션 저장소 데이터베이스로 변경 5-6 네이버 로그인 연동

--- src/main/java/com/sjacob/book/springboot/web/IndexController.java
+++ src/main/java/com/sjacob/book/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.sjacob.book.springboot.web;
 
-import com.sjacob.book.springboot.config.auth.SessionUser;
+import com.sjacob.book.springboot.config.auth.LoginUser;
+import com.sjacob.book.springboot.config.auth.dto.SessionUser;
 import com.sjacob.book.springboot.service.posts.PostsService;
 import com.sjacob.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
