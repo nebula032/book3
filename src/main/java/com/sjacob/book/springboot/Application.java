77b269a8c6d3b7f5c2cd93ced2bb94e7030a240f
sjacob <nebula032@naver.com>
2023-09-04 00:27:57 +0900

    Chapter09 9-5 배포 자동화 구성

--- src/main/java/com/sjacob/book/springboot/Application.java
+++ src/main/java/com/sjacob/book/springboot/Application.java
@@ -3,12 +3,16 @@ package com.sjacob.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
